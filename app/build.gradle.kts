ddaa6375a5a855b4ee99977049e5e112586f9e4b
Aaron Chen <tks35881@gmail.com>
2025-09-22 10:32:45 +0800

    Update app version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.aaron.chen.animeone"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
